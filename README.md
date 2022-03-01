a6895f5d6252e3847db9e471b2b55e3553b63bde
陈鹏飞 <913910173@qq.com>
2022-03-01 16:24:41 +0800

    修改README文件，修改模拟的settings数据，新增事件总线eventBus实现原理

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # 个人博客
 
-> 本项目缺少后端支持，暂时使用Mockjs来模拟数据
+> 本项目缺少后端支持，暂时使用 Mockjs 来模拟数据
 >
 > 未完待续...
-
-
 
 ## 所用技术
 
@@ -15,20 +13,30 @@
 - less
 - axios
 
+## 项目展望
+
+- 添加登录界面，使用 vue-router 的路由导航守卫实现登录鉴权
+- 后端打算使用 node 搭建轻量级服务器，为前端提供 RESTFUL 风格的 api
+- 使用 mysql 做数据持久化处理
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/mock/globalSetting.js
+++ src/mock/globalSetting.js
@@ -7,15 +7,16 @@ Mock.mock("/api/setting", "get", {
 			"https://gitee.com/Gorgeous-man/picture-library/raw/master/imgs/202201111200716.webp",
 		siteTitle: "我的个人空间",
 		github: "https://github.com/chen-123",
-		qq: "913910173",
+		qq: "123456789",
 		qqQrCode:
 			"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.xjishu.com%2Fimg%2Fzl%2F2018%2F6%2F30%2F1241359458913.gif&refer=http%3A%2F%2Fimg.xjishu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1641818453&t=e16c2ffebce2293da6122e06372f5572",
 		weixin: "gorgeous-man",
 		weixinQrCode:
 			"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbpic.588ku.com%2Felement_origin_min_pic%2F18%2F06%2F10%2F3c6fca528d38ae18bf2143777182d0f0.jpg&refer=http%3A%2F%2Fbpic.588ku.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1641818508&t=b77ee4b1df8f7923680605666a26c4a3",
-		mail: "cpf@gmail.com",
-		icp: "黑ICP备17001719号",
+		mail: "123456789@gmail.com",
+		icp: "赣ICP备17881719号",
 		githubName: "chen-123",
-		favicon: "http://mdrs.yuanjin.tech/Fs4CDlC6mwe_WXLMIiXcmSJLHO4f",
+		favicon:
+			"https://gitee.com/Gorgeous-man/picture-library/raw/master/imgs/202203011540421.ico",
 	},
 });
