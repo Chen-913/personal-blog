8814298e9766b21f24a7f55001c2efc497b0a0ee
陈鹏飞 <913910173@qq.com>
2022-05-22 22:33:38 +0800

    修复代码无法高亮渲染的问题

--- src/views/Blog/components/BlogDetail.vue
+++ src/views/Blog/components/BlogDetail.vue
@@ -1,25 +1,25 @@
 <template>
-	<div class="blog-detail-container">
-		<h1>{{ articleData.title }}</h1>
-		<div class="info">
-			<span>日期：{{ articleData.createDate | dateFormat("YYYY-MM-DD") }}</span>
-			<span>浏览：{{ articleData.scanNumber }}</span>
-			<a href="#comment-container"
-				><span>评论：{{ articleData.commentNumber }}</span></a
-			>
-			<router-link
-				:to="{
-					name: 'BlogCategory',
-					params: {
-						categoryId: articleData.category.id,
-					},
-				}"
-				><span>{{ articleData.category.name }}</span></router-link
-			>
-		</div>
-		<!-- 文章内容 -->
-		<div class="content markdown-body" v-html="articleData.htmlContent"></div>
-	</div>
+  <div class="blog-detail-container">
+    <h1>{{ articleData.title }}</h1>
+    <div class="info">
+      <span>日期：{{ articleData.createDate | dateFormat("YYYY-MM-DD") }}</span>
+      <span>浏览：{{ articleData.scanNumber }}</span>
+      <a href="#comment-container"
+        ><span>评论：{{ articleData.commentNumber }}</span></a
+      >
+      <router-link
+        :to="{
+          name: 'BlogCategory',
+          params: {
+            categoryId: articleData.category.id,
+          },
+        }"
+        ><span>{{ articleData.category.name }}</span></router-link
+      >
+    </div>
+    <!-- 文章内容 -->
+    <div class="content markdown-body" v-html="articleData.htmlContent"></div>
+  </div>
 </template>
 
 <script>
@@ -27,27 +27,32 @@
 import "@/styles/markdown.css";
 // 代码块高亮着色
 import "highlight.js/styles/atom-one-dark.css";
+// 需要引入 highlight 进行代码高亮着色
+import hljs from "highlight.js/lib/index.js";
 export default {
-	props: {
-		articleData: {
-			type: Object,
-			require: true,
-		},
-	},
+  props: {
+    articleData: {
+      type: Object,
+      require: true,
+    },
+  },
+  mounted() {
+    hljs.highlightAll();
+  },
 };
 </script>
 
 <style scoped lang="less">
 @import url("~@/styles/var.less");
 .blog-detail-container {
-	padding: 20px;
-	.info {
-		font-size: 14px;
-		color: @gray;
-		margin-bottom: 20px;
-		span {
-			margin-left: 10px;
-		}
-	}
+  padding: 20px;
+  .info {
+    font-size: 14px;
+    color: @gray;
+    margin-bottom: 20px;
+    span {
+      margin-left: 10px;
+    }
+  }
 }
 </style>
